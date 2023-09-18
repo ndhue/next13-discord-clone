a8dca3cc9a53603ddf8abe2cfcf99d7c1c2434b7
diemhue <ndhue1024@gmail.com>
2023-09-18 20:54:01 +0700

    fix socket

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  
   webpack: (config) => {
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
       bufferutil: "commonjs bufferutil",
-      swcMinify: false
     });
 
     return config;
